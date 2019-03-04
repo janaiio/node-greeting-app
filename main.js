309c720dcb6af105960bb0570e06888f3d635a17
unknown <janais1991@gmail.com>
2019-03-03 19:49:07 -0600

    Added farewells to greeting app; Included checkpoint answers .txt

---
+++ main.js
@@ -0,0 +1,12 @@
+const greetings = require('./greeting.js');
+const farewells = require('./farewell.js')
+
+greetings.greetingEnglish(); // "Hello"
+greetings.greetingJapanese(); // "Konichiwa"
+greetings.greetingSpanish(); // "Hola"
+greetings.greetingIcelandic(); // "Halló"
+
+farewells.farewellEnglish(); // "Goodbye"
+farewells.farewellJapanese(); // "Sayōnara"
+farewells.farewellSpanish(); // "Adiós"
+farewells.farewellIcelandic(); // "Bless"
